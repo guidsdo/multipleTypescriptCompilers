1538c6cce5345cde6d50c16644fda320a5ecb9d3
guidojo <guidojo@users.noreply.github.com>
2017-10-27 11:18:04 +0200

    Make debug logs more clear

--- src/debugTools.ts
+++ src/debugTools.ts
@@ -3,6 +3,7 @@ export let DEBUG_MODE = false;
 export function debugLog(message: string, argument?: any) {
     if (DEBUG_MODE) {
         argument = argument || "";
+        message = argument ? `${message};` : message;
         console.log(message, argument);
         console.log();
     }
