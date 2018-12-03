c99d0fb7d06eea20dc817fd0dfd2baf69b6a80b3
guidojo <guidojo@users.noreply.github.com>
2018-12-03 11:21:29 +0100

    Fix unit test

--- src/helpers/__tests__/fileSystemHelpers.spec.ts
+++ src/helpers/__tests__/fileSystemHelpers.spec.ts
@@ -1,23 +1,33 @@
+import * as sh from "shelljs";
+
 import { canAccessPath, findNodeModuleExecutable } from "../fileSystemHelpers";
 
-describe("findNodeModuleExecutable", () => {
-    it("finds the node module", () => {
-        expect(findNodeModuleExecutable("test_setup/folder_node_modules_present", "tsc")).toMatch(
-            /test_setup\/folder_node_modules_present\/node_modules\/.bin\/tsc$/
-        );
+const TEST_FOLDER_NODE_MODULES_PRESENT = "test_setup/folder_node_modules_present";
+const TEST_FOLDER_NODE_MODULES_MISSING = "test_setup/folder_node_modules_missing";
+
+describe("fileSystemHelpers", () => {
+    describe("findNodeModuleExecutable", () => {
+        it("finds the node module", async () => {
+            sh.mkdir("-p", `${TEST_FOLDER_NODE_MODULES_PRESENT}/node_modules/.bin`);
+            sh.touch(`${TEST_FOLDER_NODE_MODULES_PRESENT}/node_modules/.bin/tsc`);
+
+            expect(findNodeModuleExecutable(TEST_FOLDER_NODE_MODULES_PRESENT, "tsc")).toMatch(
+                /test_setup\/folder_node_modules_present\/node_modules\/.bin\/tsc$/
+            );
+        });
+
+        it("throws an error when path cannot be accessed", () => {
+            expect(() => findNodeModuleExecutable(TEST_FOLDER_NODE_MODULES_MISSING, "tsc")).toThrowError();
+        });
     });
 
-    it("throws an error when path cannot be accessed", () => {
-        expect(() => findNodeModuleExecutable("test_setup/folder_node_modules_missing", "tsc")).toThrowError();
-    });
-});
+    describe("canAccessPath", () => {
+        it("does nothing when path can be accessed", () => {
+            expect(() => canAccessPath(".")).toBeTruthy();
+        });
 
-describe("canAccessPath", () => {
-    it("does nothing when path can be accessed", () => {
-        expect(() => canAccessPath(".")).toBeTruthy();
-    });
-
-    it("throws an error when path cannot be accessed", () => {
-        expect(() => canAccessPath("fake_path")).toThrow("ENOENT: no such file or directory, access 'fake_path'");
+        it("throws an error when path cannot be accessed", () => {
+            expect(() => canAccessPath("fake_path")).toThrow("ENOENT: no such file or directory, access 'fake_path'");
+        });
     });
 });
