c0c907b7ebde681aa108b5cb50f7db859e56023e
guidojo <guidojo@users.noreply.github.com>
2018-12-20 11:14:03 +0100

    Use ts-jest for better code coverage reporting

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+module.exports = {
+    // Use ts-jest to enable correct code coverage
+    preset: "ts-jest",
+
+    // Automatically clear mock calls and instances between every test
+    clearMocks: true,
+
+    // The directory where Jest should output its coverage files
+    coverageDirectory: "coverage",
+
+    // An array of regexp pattern strings used to skip coverage collection
+    coveragePathIgnorePatterns: ["/node_modules/", "__tests__", "__mocks__"],
+
+    // A list of reporter names that Jest uses when writing coverage reports
+    coverageReporters: ["json", "text", "lcov", "clover"],
+
+    // Log all the individual tests
+    verbose: true,
+
+    // Make calling deprecated APIs throw helpful error messages
+    errorOnDeprecated: true,
+
+    // The test environment that will be used for testing
+    testEnvironment: "node",
+
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    testPathIgnorePatterns: ["/node_modules/", "/lib"]
+};
